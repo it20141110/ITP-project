1c8f04b10e48ee954150f0f88c42b83a2ca948dc
Savinya Nandakumara <86395209+IT20135720@users.noreply.github.com>
2021-09-13 19:08:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,57 @@
-import './App.css';
-import Header from './componants/header';
-import Footer from './componants/footer';
-import Adddeilery from './componants/AddDelivers';
-import AddConfOder from './componants/AddConf';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import Alldeliver from './componants/Alldivers';
-import Allconf from './componants/AllConfs';
+import React from 'react';
+import {BrowserRouter as Router,Route,Link,NavLink,Switch,useHistory} from 'react-router-dom';
+import Inventory from './components/Inventory';
+import UI from './components/UI';
+import Header from './components/Header';
+import Add from './components/Add';
+import PlantInventory from './components/PlantInventory';
+import AddPlant from './components/AddPlant';
+import AddCrop from './components/AddCrop';
+import CropInventory  from './components/CropInventory';
+import AddEquip from './components/AddEquip';
+import EquipInventory  from './components/EquipInventory';
+import AddFert from './components/AddFert';
+import FertInventory  from './components/FertInventory';
+import AddOther from './components/AddOther';
+import OtherInventory  from './components/OtherInventory';
+import Footer from './components/Footer'
 
-function App() {
+
+function App(){
+  
   return (
-    <Router>
     <div>
-      <Header/>
-      <Route path ="/add/confirm" exact component= {AddConfOder}/>
-      <Route path ="/c/" exact component={Allconf}/>
-      <Route path ="/" exact component={Alldeliver}/>
-      <Route path ="/add" exact component= {Adddeilery}/>
+    
+    
+   
+     <Router >
+       <Header/>
+  
+       <Switch>
+          <Route exact  path="/UI" component={UI}/>
+           <Route path="/inventory" component={Inventory}/>
+          <Route path="/add" component={Add}/>
+           <Route path="/addplant" component={AddPlant}/>
+           <Route path="/addcrop" component={AddCrop}/>
+           <Route path="/addequip" component={AddEquip}/>
+           <Route path="/addfert" component={AddFert}/>
+           <Route path="/addother" component={AddOther}/>
+          <Route path="/otherinventory" component={OtherInventory}/>
+          <Route path="/fertinventory" component={FertInventory}/>
+          <Route path="/equipinventory" component={EquipInventory}/>
+          <Route path="/plantinventory" component={PlantInventory}/>
+          <Route path="/cropinventory" component={CropInventory}/>
 
-      <Footer/>
-      </div>
-</Router>
+          
+
+         
+       </Switch>
+   
+    </Router>
+    <Footer/>
+    </div>
   );
+
 }
 
 export default App;
